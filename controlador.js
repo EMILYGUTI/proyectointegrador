0dd3389051494dc521b1822ec2f01603717b3b1b
desarrollo <isa87cris@gmail.com>
2025-11-24 15:12:17 -0500

    ok

---
+++ controlador.js
@@ -0,0 +1,19 @@
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+    evento.preventDefault()
+    
+    let cajausuario=document.getElementById("cajausuario")
+    let cajacorreo=document.getElementById("cajacorreo")
+    let cajaciudad=document.getElementById("cajaciudad")
+    let cajatelefono=document.getElementById("cajatelefono") 
+    let cajacontraseña=document.getElementById("cajacontraseña") 
+
+    let datosDELFORMULARIO={
+        usuario:cajausuario.value,
+        correo:cajacorreo.value,
+        ciudad:cajaciudad.value,
+        telefono:cajatelefono.value,
+        contraseña:cajacontraseña.value,
+        
+    }
+})
